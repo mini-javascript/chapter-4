58fd7d65c0a3033dabcc01c6964279a4d4ed77fd
mini-javascript <84110340+mini-javascript@users.noreply.github.com>
2023-05-23 17:22:18 +0900

    change nodemon version & index.js request path

--- example-2/video-streaming/src/index.js
+++ example-2/video-streaming/src/index.js
@@ -8,15 +8,21 @@ const app = express();
 //
 
 if (!process.env.PORT) {
-    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
 }
 
 if (!process.env.VIDEO_STORAGE_HOST) {
-    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+  throw new Error(
+    "Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST."
+  );
 }
 
 if (!process.env.VIDEO_STORAGE_PORT) {
-    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+  throw new Error(
+    "Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT."
+  );
 }
 
 //
@@ -25,32 +31,35 @@ if (!process.env.VIDEO_STORAGE_PORT) {
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
-console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+console.log(
+  `Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`
+);
 
 //
 // Registers a HTTP GET route for video streaming.
 //
 app.get("/video", (req, res) => {
-    const forwardRequest = http.request( // Forward the request to the video storage microservice.
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4', // Video path is hard-coded for the moment.
-            method: 'GET',
-            headers: req.headers
-        }, 
-        forwardResponse => {
-            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-            forwardResponse.pipe(res);
-        }
-    );
-    
-    req.pipe(forwardRequest);
+  const forwardRequest = http.request(
+    // Forward the request to the video storage microservice.
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: "/video/m01.mp4", // Video path is hard-coded for the moment.
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardResponse) => {
+      res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+      forwardResponse.pipe(res);
+    }
+  );
+
+  req.pipe(forwardRequest);
 });
 
 //
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
-    console.log(`Microservice online`);
+  console.log(`Microservice online`);
 });
